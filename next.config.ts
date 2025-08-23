1ecd2b8c5bfbc2aa4fecce6f81a6a062a732dfb9
Ashish Kumar Singh <118729511+Ashish001sing@users.noreply.github.com>
2025-08-24 01:09:38 +0530

    Remove unused domain from images configuration

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      "img.youtube.com",
-      "images.ctfassets.net",
-      "assets.ctfassets.net"
-    ],
+   domains: [
+"img.youtube.com",
+"images.ctfassets.net"
+ ],
   },
 };
 
