0aba3ec5a74a5035d2e2916a8fccc926f0892134
Ashish Kumar Singh <118729511+Ashish001sing@users.noreply.github.com>
2025-08-24 00:53:08 +0530

    Add additional image domains to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["img.youtube.com"],
+    domains: [
+      "img.youtube.com",
+      "images.ctfassets.net",
+      "assets.ctfassets.net"
+    ],
   },
 };
 
