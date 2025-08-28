2360cc2eb7318f56a7a3e889e120d3b242a2ed85
sachan13harshit <harshit.23bcs10134@sst.scaler.com>
2025-08-28 21:13:01 +0530

    fix the navbar overlapping in ipad

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -22,10 +22,11 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3 md:px-2 lg:px-4",
+        default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
+        adaptive: "h-9 px-4 py-2 has-[>svg]:px-3 md:px-0 lg:px-4",
       },
     },
     defaultVariants: {
